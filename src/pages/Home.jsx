88480674a82ce1c887c2946b2baefdec8cb36951
Mariah <alinemariah@hotmail.com>
2025-09-01 23:29:06 +0200

    add Tasks Form, Item and List components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,12 @@ const Home = () => {
         className='relative min-h-screen bg-fixed bg-cover bg-center'
         >
             {/* <img src={imgbg} alt="" /> */}
+        <Link>
+        
+         <h1> Home
+            </h1>   
+
+        </Link>    
             
 
         </div>
